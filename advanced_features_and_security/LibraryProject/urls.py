20ebd63a471919bb27ab14dcc2fe694120cd408e
Benjamin W. Pixel <benjamin.w.pixel@example.com>
2025-08-07 17:32:54 +0300

    this is adnvaced features and security

--- advanced_features_and_security/LibraryProject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # Make sure to import include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('relationship/', include('relationship_app.urls')),  # Include your app's URLs
-]
--- advanced_features_and_security/users/models.py
+++ advanced_features_and_security/users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
+import os
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
