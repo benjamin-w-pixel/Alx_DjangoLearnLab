9027aa8c0fe3be809ebfcf0b16b4ccda6623ad8f
Benjamin W. Pixel <benjamin.w.pixel@example.com>
2025-08-08 16:10:51 +0300

    this is adnvaced features and security

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
+class CustomUserAdmin(UserAdmin):
+    # Define the admin interface for CustomUser
+    model = CustomUser
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'date_of_birth', 'is_staff', 'is_superuser'),
+        }),
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
+    admin.site.register(CustomUser, CustomUserAdmin)
+    
+# Register the CustomUser model with its admin interface
+admin.site.register(CustomUser, CustomUserAdmin)
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # Columns to display
