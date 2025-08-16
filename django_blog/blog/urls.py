e9620f60eee7addd101a1dbb2a7a5264caf5c154
Benjamin W. Pixel <benjamin.w.pixel@example.com>
2025-08-16 15:19:54 +0300

    this is django blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     
     # Comment URLs using class-based views
-    path('post/<int:pk>/comment/', CommentCreateView.as_view(), name='comment-create'),
+   path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     
