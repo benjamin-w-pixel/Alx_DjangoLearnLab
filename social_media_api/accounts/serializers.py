d71477cec7db375b26930f3e7531f375d5170583
Benjamin W. Pixel <benjamin.w.pixel@example.com>
2025-08-22 18:15:55 +0300

    this the projet for rest framework in djnago

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,23 +4,17 @@ from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
-    token = serializers.SerializerMethodField(read_only=True)
-    followers = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-
-    def get_token(self, obj):
-        token, _ = Token.objects.get_or_create(user=obj)
-        return token.key
-
-    def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
-
+class RegisterSerializer (serializers.ModelSerializer):
+    password = serializers.CharField (required=True, write_only=True)
     class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'password', 'bio', 'profile_picture', 'followers', 'token')
-        read_only_fields = ('id', 'token')
+        model = get_user_model ()
+        fields = ('username','email','password')
+
+    def create (self,validated_data):
+        user = get_user_model ().objects.create_user (
+            username=validated_data ['username'],
+            email=validated_data ['email'],
+            password=validated_data ['password'],
+        )
+        Token.objects.create (user=user)
+        return user
