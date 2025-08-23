e12fb63a692968f90e8d0aa2d1645cb39f33cf5f
Benjamin W. Pixel <benjamin.w.pixel@example.com>
2025-08-23 19:11:54 +0300

    this is project of social_media_app

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -150,3 +151,5 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_SSL_REDIRECT = True
 # Port configuration for deployment
 PORT = int(os.environ.get('PORT', 8000))
+# Static files configuration
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
