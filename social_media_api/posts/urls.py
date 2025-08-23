0b775b8b0213686bfbe88cdd48f5f8761d366e19
Benjamin W. Pixel <benjamin.w.pixel@example.com>
2025-08-23 17:41:30 +0300

    this is project of social_media_app

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import SimpleRouter
-from .views import PostViewSet
-from django.urls import path, include
+from .views import FeedView, PostViewSet
+from django.urls import path
 router = SimpleRouter()
 router.register(r'posts', PostViewSet, basename='post')
 
 urlpatterns = [
     path('posts/<int:id>/like/', PostViewSet.as_view({'post': 'like'}), name='post-like'),
+    path("feed/", FeedView.as_view(), name="feed"),
     *router.urls,
 ]
