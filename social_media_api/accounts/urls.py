fb5f43580d973219509e492ca07d286da3b00964
Benjamin W. Pixel <benjamin.w.pixel@example.com>
2025-08-23 17:37:33 +0300

    this is project of social_media_app

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import RegisterView, LoginView, ProfileView
-
+from .views import FollowView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('unfollow/<int:user_id>/', FollowView.as_view(), name='unfollow'),
+    path('follow/<int:user_id>/', FollowView.as_view(), name='follow'),
+    
 ]
